ceed5d8d8fe9e400f53b6b168ceb664951057a68
Mnkith <munkithabid@gmail.com>
2020-12-24 20:42:24 -0600

    commit tree 50f3ad4082f222eea4129ea0f693ba0a6393b819

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,28 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    student = Student.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
-  def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+  def self.run(query, *args) #programers are lazy, and variable arguments is cool
+    DB[:conn].execute(query, args).map{|row| self.new_from_db(row)}
   end
 
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+  def self.all 
+    sql = "SELECT * FROM students"
+    run(sql)
+  end
+
+  def self.find_by_name(name) #Don't know why we used LIMIT 1 in the lab even thu the query returns one student
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+    run(sql, name)[0]
   end
   
   def save
@@ -33,11 +44,46 @@ class Student
     )
     SQL
 
-    DB[:conn].execute(sql)
+    run(sql)
   end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
+    run(sql)
   end
+
+  def self.all_students_in_grade_9
+    all_students_in_grade_X(9)
+  end
+
+  def self.students_below_12th_grade
+    all.select{|st| st.grade.to_i < 12}
+    # sql = <<-SQL
+    #   SELECT * FROM students
+    #   WHERE grade < ?
+    # SQL
+    # run(sql, 12)
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    all_students_in_grade_X(10).take(x)
+    # sql = <<-SQL
+    #   SELECT * FROM students
+    #   WHERE grade = 10
+    #   LIMIT ?
+    # SQL
+    # run(sql, x)
+  end
+
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1)[0]
+  end
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
+    run(sql, x)
+  end
+
 end
